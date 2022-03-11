27134bf12046d110309696d3b87b6255fa5c9399
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-03-11 17:15:37 +0300

    Update Main.kt
    - implemented photo inversion.

--- Main.kt
+++ Main.kt
@@ -5,7 +5,33 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-fun main() {
+private const val IN_ARGUMENT = "-in"
+private const val OUT_ARGUMENT = "-out"
+
+fun main(args: Array<String>) {
+    invertPhoto(args)
+//    drawCross()
+}
+
+private fun invertPhoto(args: Array<String>) {
+    val inputFileName = if (args[0] == IN_ARGUMENT) args[1] else "in.png"
+    val outputFileName = if (args[2] == OUT_ARGUMENT) args[3] else "out.png"
+    val imageFile = File(inputFileName)
+    val file = ImageIO.read(imageFile)
+
+    val image = BufferedImage(file.width, file.height, BufferedImage.TYPE_INT_RGB)
+    for (x in 0 until file.width) {
+        for (y in 0 until file.height) {
+            val color = Color(file.getRGB(x, y))
+            val modifiedColor = Color(255 - color.red, 255 - color.green, 255 - color.blue)
+            image.setRGB(x, y, modifiedColor.rgb)
+        }
+    }
+    val outputFile = File(outputFileName)
+    ImageIO.write(image, "png", outputFile)
+}
+
+private fun drawCross() {
     println("Enter rectangle width:")
     val width = readLine()!!.toInt()
     println("Enter rectangle height:")
@@ -17,7 +43,7 @@ fun main() {
     val graphics = image.createGraphics()
     graphics.background = Color.BLACK
     graphics.color = Color.BLACK
-    graphics.fillRect(0, 0 , width, height)
+    graphics.fillRect(0, 0, width, height)
     graphics.color = Color.RED
     graphics.drawLine(0, 0, width - 1, height - 1)
     graphics.drawLine(-1, height, width - 1, 0)
